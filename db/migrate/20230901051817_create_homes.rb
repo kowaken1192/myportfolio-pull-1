1efc2df188612d4611fd313a56f25348082e21fe
小若憲史 <kenjikowaka@kowakaknjinoMBP.lan>
2023-09-01 14:29:16 +0900

    devise導入

---
+++ db/migrate/20230901051817_create_homes.rb
@@ -0,0 +1,10 @@
+class CreateHomes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :homes do |t|
+      t.string :first_name
+      t.string :last_name
+
+      t.timestamps
+    end
+  end
+end
