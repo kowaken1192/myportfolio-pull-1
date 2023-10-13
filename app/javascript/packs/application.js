6cb7ed7483f58f65a907494f62a79fc8161281d6
小若憲史 <kenjikowaka@kowakakenjinoMacBook-Pro.local>
2023-10-13 17:59:23 +0900

    レスポンシブ調整、ナビゲーション画面レスポンシブ時変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,3 +13,28 @@ Turbolinks.start()
 ActiveStorage.start()
 
 import "./search.post"
+
+document.addEventListener('DOMContentLoaded', () => {
+  const navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  if (navbarBurgers.length > 0) {
+    navbarBurgers.forEach(el => {
+      el.addEventListener('click', () => {
+        const target = el.dataset.target;
+        const targetElement = document.getElementById(target);
+        el.classList.toggle('is-active');
+        targetElement.classList.toggle('is-active');
+      });
+    });
+  }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const navbarLink = document.querySelector('.navbar-link');
+  
+  navbarLink.addEventListener('click', (event) => {
+    if (window.innerWidth <= 1023) { 
+      event.preventDefault(); // イベントのデフォルトの動作を停止
+      event.stopPropagation(); // イベントの伝播を停止
+    }
+  });
+});
