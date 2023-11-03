f71e54cebad804f031705fc4a38e7a7c2030702b
小若憲史 <kenjikowaka@kowakakenjinoMacBook-Pro.local>
2023-11-03 17:30:29 +0900

    タイムゾーン日本語に変更

--- config/application.rb
+++ config/application.rb
@@ -2,19 +2,30 @@ require_relative "boot"
 
 require "rails/all"
 
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
 module LikeApp
   class Application < Rails::Application
+    config.load_defaults 6.0
+    # 日本語化の設定
+    config.i18n.default_locale = :ja
+    # タイムゾーンの変更（例)created_at カラムを取り出したときに日本時間に変換されるようになる） 
+    config.time_zone = 'Asia/Tokyo'
+  end
+end
+
+module Myapp
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
-    # 英語化の設定
-    config.i18n.default_locale = :ja
-
-    # タイムゾーンの変更（例：created_at カラムを取り出したときに日本時間に変換されるようになる）
-    config.time_zone = 'Asia/Tokyo'
-
-    # その他の設定
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
