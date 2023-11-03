8347bb8ecee911071ab9a848bb4bb8f3664b8318
小若憲史 <kenjikowaka@kowakakenjinoMacBook-Pro.local>
2023-11-03 16:49:31 +0900

    map controller追加

--- test/controllers/map_controller_test.rb
+++ test/controllers/map_controller_test.rb
@@ -1,13 +1,7 @@
 require "test_helper"
 
 class MapControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get map_index_url
-    assert_response :success
-  end
-
-  test "should get search" do
-    get map_search_url
-    assert_response :success
-  end
+  # test "the truth" do
+  #   assert true
+  # end
 end
