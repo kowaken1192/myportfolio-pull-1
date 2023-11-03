ac99fcdb2f1b2ef52e80812622e25efa01a7bda2
小若憲史 <kenjikowaka@kowakakenjinoMacBook-Pro.local>
2023-11-03 19:32:45 +0900

    applicaton.rb変更

--- config/application.rb
+++ config/application.rb
@@ -5,15 +5,6 @@ require "rails/all"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-module LikeApp
-  class Application < Rails::Application
-    config.load_defaults 6.0
-    # 日本語化の設定
-    config.i18n.default_locale = :ja
-    # タイムゾーンの変更（例)created_at カラムを取り出したときに日本時間に変換されるようになる） 
-    config.time_zone = 'Asia/Tokyo'
-  end
-end
 
 module Myapp
   class Application < Rails::Application
