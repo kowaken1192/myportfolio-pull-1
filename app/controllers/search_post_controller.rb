00c2288f87a4a38b03673ab7626a203aed27a39e
小若憲史 <kenjikowaka@kowakakenjinoMacBook-Pro.local>
2023-11-04 18:40:58 +0900

    検索結果画面でも人気投稿などが見れるように実装

--- app/controllers/search_post_controller.rb
+++ app/controllers/search_post_controller.rb
@@ -8,10 +8,19 @@ class SearchPostController < ApplicationController
   
   def show
     @q = Post.ransack(params[:q])
-    @results = @q.result(distinct: true).includes(:reviews).order(created_at: :desc)
+    @results = @q.result(distinct: true)
+    @results = @results.with_counts.with_avg_score 
+    if params[:sort_by] == 'avg_score_and_review_count'
+      @results = @results.avg_score_and_review_count
+    elsif params[:latest]
+      @results = @results.latest
+    elsif params[:reviews_count]
+      @results = @results.reviews_count
+    end
+    @favorited_post_ids = current_user.favorites.pluck(:post_id)
     @prefecture_name = params[:q][:address_cont] if params[:q] && params[:q][:address_cont]
   end
-
+  
   def count_by_prefecture
     counts = Post.group(:prefecture).count
     render json: counts
