55380f22ee75bedb255f4c83d84584e6b46ddd1c
小若憲史 <kenjikowaka@kowakaknjinoMBP.lan>
2023-11-07 20:09:55 +0900

    ENVをENV.fetchに変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,10 +8,9 @@ CarrierWave.configure do |config|
   config.fog_directory  = 'kowakentimes'
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region: ENV['AWS_DEFAULT_REGION'],
+    aws_access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    region: ENV.fetch('AWS_DEFAULT_REGION'),
     path_style: true
   }
-
 end
