046103abc52f62cdcede5138a72c9a9c3393efad
小若憲史 <kenjikowaka@kowakaknjinoMBP.lan>
2023-11-16 15:53:39 +0900

    emailの重複チェックとpassword_confirmationのバリデーションテストを追加。let!を用いて変数を定義

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,36 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # first_nameがなければ無効な状態であること
+  let!(:user) { FactoryBot.create(:user) }
+  
+  #first_nameがなければ無効な状態であること
   it "is invalid without a first name" do
-    user = FactoryBot.build(:user, first_name: nil)
+    user.first_name = nil
     user.valid?
     expect(user.errors[:first_name]).to include("を入力してください")
   end
 
-  # last_nameがなければ無効な状態であること
+  #last_nameがなければ無効な状態であること
   it "is invalid without a last name" do
-    user = FactoryBot.build(:user, last_name: nil)
+    user.last_name = nil
     user.valid?
     expect(user.errors[:last_name]).to include("を入力してください")
   end
-
-  # emailがなければ無効な状態であること
+  
+  #emailがなければ無効な状態であること
   it "is invalid without an email address" do
-    user = FactoryBot.build(:user, email: nil)
+    user.email = nil
     user.valid?
     expect(user.errors[:email]).to include("を入力してください")
   end
-
-  # パスワードがなければ無効な状態であること
-  it "is invalid without an password" do
+  
+  #重複したemailなら無効な状態であること
+  it "is invalid with a duplicate email address" do
+    another_user = build(:user, email: user.email)
+    another_user.valid?
+    expect(another_user.errors[:email]).to include("はすでに存在します")
+  end
+  
+  #passwordがなければ無効な状態であること
+  it "is invalid without a password" do
     user = FactoryBot.build(:user, password: nil)
     user.valid?
     expect(user.errors[:password]).to include("を入力してください")
   end
-
+  
+  #passwordが存在してもpassword_confirmationがなければ無効な状態であること
+  it "is invalid without a password_confirmation although with a password" do
+    user = FactoryBot.build(:user, password_confirmation: "")
+    user.valid?
+    expect(user.errors[:password_confirmation]).to include("とパスワードの入力が一致しません")
+  end
+  
+  # Userモデルが複数のreviewsとの関連を持っていることをテストし、Userが削除された場合に関連するreviewsも削除されることを確認
   it { should have_many(:reviews).dependent(:destroy) }
+  # Userモデルが複数のfavoritesとの関連を持っていることをテストし、Userが削除された場合に関連するfavoritesも削除されることを確認
   it { should have_many(:favorites).dependent(:destroy) }
+  # Userモデルがfavoritesを通じてfavorite_postsと関連していることをテストし、この関連がpostモデルに源を発していることを確認
   it { should have_many(:favorite_posts).through(:favorites).source(:post) }
+  # Userモデルが複数のpostsとの関連を持っていることをテストし、Userが削除された場合に関連するpostsも削除されることを確認
   it { should have_many(:posts).dependent(:destroy) }
 end
