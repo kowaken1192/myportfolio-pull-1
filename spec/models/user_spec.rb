cc2d9dc9b89551e54aca5dd6d695d49c944c7b99
小若憲史 <kenjikowaka@kowakaknjinoMBP.lan>
2023-11-18 16:45:21 +0900

    Userモデルの認証関連メソッドのテストを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,13 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let!(:user) { FactoryBot.create(:user) }
+  let!(:user_with_duplicate_email) { FactoryBot.build(:user, email: user.email) }
+  let!(:user_without_password) { FactoryBot.build(:user, password: nil) }
+  let!(:user_without_password_confirmation) { FactoryBot.build(:user, password_confirmation: "") }
   let!(:under_six_password) { FactoryBot.build(:user, password: '12345', password_confirmation: '12345') }
+  let!(:valid_user) { FactoryBot.create(:user, is_valid: true) }
+  let!(:invalid_user) { FactoryBot.create(:user, is_valid: false) }
+  let!(:valid_user) { FactoryBot.create(:user, is_valid: true) }
 
   it "first_nameがなければ無効な状態であること" do
     user.first_name = nil
@@ -23,28 +29,25 @@ RSpec.describe User, type: :model do
   end
   
   it "重複したemailなら無効な状態であること" do
-    another_user = build(:user, email: user.email)
-    another_user.valid?
-    expect(another_user.errors[:email]).to include("はすでに存在します")
+    user_with_duplicate_email.valid?
+    expect(user_with_duplicate_email.errors[:email]).to include("はすでに存在します")
   end
   
   it "passwordがなければ無効な状態であること" do
-    user = FactoryBot.build(:user, password: nil)
-    user.valid?
-    expect(user.errors[:password]).to include("を入力してください")
+    user_without_password.valid?
+    expect(user_without_password.errors[:password]).to include("を入力してください")
   end
   
   it "passwordが存在してもpassword_confirmationがなければ無効な状態であること" do
-    user = FactoryBot.build(:user, password_confirmation: "")
-    user.valid?
-    expect(user.errors[:password_confirmation]).to include("とパスワードの入力が一致しません")
+    user_without_password_confirmation.valid?
+    expect(user_without_password_confirmation.errors[:password_confirmation]).to include("とパスワードの入力が一致しません")
   end
   
   it "パスワードが6文字未満だと無効な状態であること" do
     under_six_password.valid?
     expect(under_six_password.errors[:password]).to include("は6文字以上で入力してください")
   end
-
+  
   describe 'Guest' do
     context 'ゲストユーザーが存在しない場合' do
       it '新しいゲストユーザーに正しい属性を設定する' do
@@ -53,6 +56,34 @@ RSpec.describe User, type: :model do
         expect(guest_user.first_name).to eq('Guest')
         expect(guest_user.last_name).to eq('User')
         expect(guest_user.is_valid).to be true
+      end
+    end
+  end
+
+  describe '#active_for_authentication?' do
+    context 'ユーザーが有効な場合' do
+      it 'trueを返すこと' do
+        expect(valid_user.active_for_authentication?).to be true
+      end
+    end
+
+    context 'ユーザーが無効な場合' do
+      it 'falseを返すこと' do
+        expect(invalid_user.active_for_authentication?).to be false
+      end
+    end
+  end
+
+  describe '#inactive_message' do
+    context 'ユーザーが有効な場合' do
+      it 'デフォルトのメッセージを返すこと' do
+        expect(valid_user.inactive_message).to eq :inactive
+      end
+    end
+
+    context 'ユーザーが無効な場合' do
+      it 'カスタムメッセージを返すこと' do
+        expect(invalid_user.inactive_message).to eq :account_not_valid
       end
     end
   end
