85c1889614bfc1ff7299bb21986afe9ab8777cd6
小若憲史 <kenjikowaka@kowakaknjinoMBP.lan>
2023-11-21 18:13:27 +0900

    favoriteモデルのテスト完成(user_idとpost_id野一意性のテスト追加)

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Favorite, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    let!(:user) { FactoryBot.create(:user) }
+    let!(:post) { FactoryBot.create(:post) }
+    let!(:favorite) { FactoryBot.create(:favorite, user_id: user.id, post_id: post.id) }
+
+    it 'user_idのスコープ内でpost_idの一意性を検証する' do
+      expect(favorite).to validate_uniqueness_of(:post_id).scoped_to(:user_id)
+    end
+  end
 end
