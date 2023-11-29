7bde9c60998bd8dde42a17337a3b0999754dabe4
小若憲史 <kenjikowaka@kowakakenjinoMacBook-Pro.local>
2023-11-29 18:49:04 +0900

    保守性のためuser/specとfavorite/specを分ける

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -160,39 +160,4 @@ RSpec.describe "Users", type: :system do
       expect(page).to have_content "名前を入力してください"
     end
   end
-
-  describe "Favorites", type: :system do
-    let!(:favorite_posts) { create_list(:post, 2, user: user, postimage: Rack::Test::UploadedFile.new('spec/fixtures/test.jpeg', 'image/jpeg')) }
-
-    before do
-      favorite_posts.each { |post| user.favorites.create(post: post) }
-      @favorite_posts = favorite_posts
-      @favorited_post_ids = favorite_posts.map(&:id)
-      visit favorites_user_path(user)
-    end
-  
-    it '必要な投稿内容の情報が表示されていること' do
-      @favorite_posts.each do |post|
-        expect(page).to have_content(post.name)
-        expect(page).to have_content(post.address)
-        expect(page).to have_content(post.created_at.strftime('%Y年 %m月%d日'))
-      end
-    end
-  
-    it 'お気に入りアイコンが正しく表示されていること' do
-      @favorite_posts.each do |post|
-        if @favorited_post_ids.include?(post.id)
-          expect(page).to have_css("i.fas.fa-heart")
-        else
-          expect(page).to have_css("i.far.fa-heart")
-        end
-      end
-    end
-
-    it 'レビュースコアが正しく表示されていること' do
-      @favorite_posts.each do |post|
-        expect(page).to have_css('.star-rating-front', style: "width: #{post.review_score_percentage}%")
-      end
-    end
-  end
 end
