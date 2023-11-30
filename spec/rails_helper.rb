9b42729f345982166e54c75c9421b3e9f69492a9
小若憲史 <kenjikowaka@kowakakenjinoMacBook-Pro.local>
2023-12-01 01:20:22 +0900

    'rails-controller-testing'を追加し、RSpec設定にDeviseのテストヘルパーを追加

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -42,6 +42,7 @@ RSpec.configure do |config|
   config.fixture_path = Rails.root.join('spec/fixtures')
   
   config.include Devise::Test::IntegrationHelpers, type: :system
+  config.include Devise::Test::IntegrationHelpers, type: :request
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
