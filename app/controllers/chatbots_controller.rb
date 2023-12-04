39f081851f7d8d3354fcf3cd0d45ecc7ad9c2179
小若憲史 <kenjikowaka@kowakaknjinoMBP.lan>
2023-12-04 17:08:53 +0900

    無駄な変数割り当てを削除し、環境変数の取得方法を修正

--- app/controllers/chatbots_controller.rb
+++ app/controllers/chatbots_controller.rb
@@ -1,16 +1,17 @@
 class ChatbotsController < ApplicationController
   def create
     input = params[:input] # ユーザーからの入力テキストです
-    client = OpenAI::Client.new(access_token: ENV["OPENAI_API_KEY"]) # OpenAI APIを利用するためのインスタンスを生成します
+    client = OpenAI::Client.new(access_token: ENV.fetch("OPENAI_API_KEY")) # OpenAI APIを利用するためのインスタンスを生成します
     
     response = client.chat(
-    parameters: {
+      parameters: {
         model: "gpt-3.5-turbo", # 利用するModelを指定しています
-        messages: [{ role: "user", content: input}], # OpenAIへ送信する内容を指定しています
+        messages: [{ role: "user", content: input }], # OpenAIへ送信する内容を指定しています
         max_tokens: 150
-    })
+      }
+    )
     
-    trimmed_response = response.dig("choices", 0, "message", "content").truncate(200)
+    response.dig("choices", 0, "message", "content").truncate(200)
     render json: { response: response.dig("choices", 0, "message", "content") } # レスポンスをViewへ返却しています
   end
 end
