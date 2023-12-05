312e9036b05e2dc475fe7b6eb926a62cecb6a072
小若憲史 <kenjikowaka@kowakakenjinoMacBook-Pro.local>
2023-12-05 17:54:25 +0900

    posts.jsの不要コード削除

--- app/javascript/packs/posts.js
+++ app/javascript/packs/posts.js
@@ -1,9 +1,12 @@
+// ドキュメントが読み込まれた際の処理
 document.addEventListener("DOMContentLoaded", function() {
+  // クッキーから保存した住所を取得
   let savedAddress = getCookie("selectedAddress");
   let savedCountry = getCookie("selectedCountry");
   let inputPlace = getCookie("inputPlace");
   let savedPrefecture = getCookie("selectedPrefecture");
 
+  // 保存した住所があれば、対応する入力フィールドに値を設定
   if (savedAddress) {
     savedAddress = removeLeadingPunctuation(decodeURIComponent(savedAddress));
     document.getElementById("post_address").value = savedAddress;
@@ -21,10 +24,11 @@ document.addEventListener("DOMContentLoaded", function() {
 
   if (savedPrefecture) {
     savedPrefecture = removeLeadingPunctuation(decodeURIComponent(savedPrefecture));
-    document.getElementById("post_prefecture").value = savedPrefecture; // あなたの都道府県のinput要素のIDに合わせてください
+    document.getElementById("post_prefecture").value = savedPrefecture;
   }
 });
 
+// 文字列の先頭の句読点を削除する関数
 function removeLeadingPunctuation(string) {
   if (!string) return string;
   while (string.startsWith('、')) {
@@ -33,28 +37,20 @@ function removeLeadingPunctuation(string) {
   return string;
 }
 
+// クッキーから値を取得する関数
 function getCookie(name) {
   let value = "; " + document.cookie;
   let parts = value.split("; " + name + "=");
   if (parts.length == 2) return parts.pop().split(";").shift();
 }
 
+// マップページにリダイレクトする関数
 function redirectToMapPage() {
   window.location.href = "/map/index";
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
-    const $notification = $delete.parentNode;
-
-    $delete.addEventListener('click', () => {
-      $notification.parentNode.removeChild($notification);
-    });
-  });
-});
-
+// Google Mapsの初期化
 function initMap() {
-
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 14, // ズームレベルを調整できます
     center: spot
