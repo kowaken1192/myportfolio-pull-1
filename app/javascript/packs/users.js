573d9c823d0401186d78fa4edfd2d94af5fbefe8
小若憲史 <kenjikowaka@kowakaknjinoMBP.lan>
2023-12-06 17:50:24 +0900

    その他のjavascriptコードにもコメントラインを追加

--- app/javascript/packs/users.js
+++ app/javascript/packs/users.js
@@ -1,8 +1,13 @@
+// 'DOMContentLoaded' イベントが発生したときに実行される関数を設定
 document.addEventListener('DOMContentLoaded', () => {
+  // ドキュメント内のすべての '.notification .delete' 要素を取得し、それらに対して処理を行う
   (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+    // 削除ボタンの親要素（通知要素）を取得
     const $notification = $delete.parentNode;
 
+    // 削除ボタンにクリックイベントリスナーを追加
     $delete.addEventListener('click', () => {
+      // クリックされたときに通知要素をDOMから削除
       $notification.parentNode.removeChild($notification);
     });
   });
