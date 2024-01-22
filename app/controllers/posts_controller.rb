53bef0bb46ae45c0aa8f6aaa8311ca15a57a3b28
小若憲史 <kenjikowaka@kowakakenjinoMacBook-Pro.local>
2024-01-22 18:59:30 +0900

    application_controllerでログイン規制を入れたので各コントローラーでのbefore_action :authenticate_userを削除

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,4 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: [:index]
   before_action :set_post, only: [:destroy, :related, :all_reviews]
 
   def index
