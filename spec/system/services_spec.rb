ae09e5d54fa584bed4d035e1054c1fa317c5c2d3
小若憲史 <kenjikowaka@kowakakenjinoMacBook-Pro.local>
2024-01-22 17:27:17 +0900

    翻訳画面のシステムスペックテストエラー解決

--- spec/system/services_spec.rb
+++ spec/system/services_spec.rb
@@ -4,6 +4,12 @@ RSpec.describe "Services", type: :system do
   let(:user) { create(:user) }
 
   before do
+    visit new_user_session_path
+    within(".box") do
+      fill_in 'user[email]', with: user.email
+      fill_in 'user[password]', with: user.password
+      click_button 'ログイン'
+    end
     visit service_index_path
   end
 
