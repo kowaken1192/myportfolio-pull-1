9092953e1435cd34f027a593b1fbc4ff6beb5035
小若憲史 <kenjikowaka@kowakakenjinoMacBook-Pro.local>
2024-01-24 12:35:30 +0900

    scoreカラムにもnull:falseを指定する

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_01_23_105755) do
+ActiveRecord::Schema.define(version: 2024_01_24_033235) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id", null: false
